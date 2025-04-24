271e545b79a11e5d0bbb2214da57d6f47ea8baaf
nathalia barcellos <barcellosqa@gmail.com>
2025-04-24 16:08:18 -0300

    repositório cypress

---
+++ teste.js
@@ -0,0 +1,8 @@
+// Código simples para exibir uma mensagem e interagir com o usuário
+function saudarUsuario(nome) {
+    console.log("Olá, " + nome + "! Bem-vindo ao mundo do JavaScript.");
+  }
+  
+  // Solicitar o nome do usuário
+  let nomeUsuario = prompt("Digite seu nome:");
+  saudarUsuario(nomeUsuario);
